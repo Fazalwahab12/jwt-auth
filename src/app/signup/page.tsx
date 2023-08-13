1aff2ce79eb712a766bf57c07fe481cab835312c
Fazal wahab <wahabdir567@gamil.com>
2023-08-13 05:17:11 -0700

    changes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,51 +1,63 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation'; // Correct import path for router
+import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import Link from 'next/link';
 import { toast } from 'react-toastify';
-
-export default function Login() { // Capitalized component name
+export default function SignupPage() {
     const router = useRouter();
-    const [user, setUser] = useState({
-        email: '',
-        password: '',
-    });
-    const [buttonDisabled, setButtonDisabled] = useState(false); // Correct usage of useState
-    const [loading, setLoading] = useState(false); // Correct usage of useState
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
-    const onLogin = async () => { // Renamed function to onLogin
+    const onSignup = async () => {
         try {
             setLoading(true);
-            const response = await axios.post("/api/login", user);
-            console.log("Login success", response.data);
-            toast.success("Login successful! Redirecting to profile...");
-            setTimeout(() => {
-                router.push("/profile");
-            }, 2000);
+            const response = await axios.post("/api/signup", user);
+            console.log("Signup success", response.data);
+            router.push("/login");
+            
         } catch (error:any) {
-            console.log("Login failed", error.message);
-            toast.error(`Login failed: ${error.message}`);
-        } finally {
+            console.log("Signup failed", error.message);
+            
+            toast.error(error.message);
+        }finally {
             setLoading(false);
         }
     }
 
     useEffect(() => {
-        if (user.email.length > 0 && user.password.length > 0) {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
             setButtonDisabled(false);
         } else {
             setButtonDisabled(true);
         }
     }, [user]);
 
+
     return (
         <div className="flex flex-col items-center mt-8">
-            <h1 className="text-2xl font-bold">{loading ? 'processing' : 'login'}</h1>
+            <h1 className="text-2xl font-bold">{loading ? "procising" : "singup"}</h1>
 
             <hr className="w-full my-4" />
 
             <div className="w-64">
+                <label htmlFor="username" className="block mb-1 font-medium text-gray-700">
+                    Username
+                </label>
+                <input
+                    id="username"
+                    type="text"
+                    placeholder="Username"
+                    value={user.username}
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
+                    className="w-full px-4 py-2 mb-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                />
+
                 <label htmlFor="email" className="block mb-1 font-medium text-gray-700">
                     Email
                 </label>
@@ -71,19 +83,18 @@ export default function Login() { // Capitalized component name
                 />
 
                 <button
-                    onClick={onLogin}
-                    disabled={buttonDisabled || loading}
-                    className={`w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg ${buttonDisabled || loading ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600 focus:outline-none"} transition duration-300`}
+                    onClick={onSignup}
+                    className="w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none"
                 >
-                    {loading ? "Processing" : "Login"}
+                 {buttonDisabled ? "No signup" : "Signup"}
                 </button>
             </div>
 
-            <hr className="w-full my-4" />
+       
 
-            <Link href="/signup" className="text-blue-500 hover:underline">
-                Not registered? Sign up here
+            <Link href="login" className="text-blue-500 hover:underline">
+                Already registered? Log in here
             </Link>
         </div>
     );
-}
+} 
